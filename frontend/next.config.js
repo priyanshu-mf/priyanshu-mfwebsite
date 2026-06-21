2b611168ed36ffc6e42a7b5fc6ed163cf7316910
priyanshu-mf <priyanshu.mf.2007@gmail.com>
2026-06-22 01:06:54 +0530

    build: Add frontend next.config.js

---
+++ frontend/next.config.js
@@ -0,0 +1,24 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ['res.cloudinary.com'],
+    unoptimized: false,
+  },
+  headers: async () => {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'X-DNS-Prefetch-Control',
+            value: 'on',
+          },
+        ],
+      },
+    ]
+  },
+}
+
+module.exports = nextConfig
